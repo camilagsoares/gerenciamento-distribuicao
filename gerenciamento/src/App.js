a29ec424c6d5504c52a2bd00031280b04418847c
camilagsoares <camila.soares19@hotmail.com>
2024-01-29 09:52:53 -0300

    .

--- gerenciamento/src/App.js
+++ gerenciamento/src/App.js
@@ -1,4 +1,3 @@
-import './App.css';
 import { GlobalStyles } from "./styles/global";
 import Login from './pages/Login/Login';
 import { BrowserRouter as Router, Routes, Route, } from 'react-router-dom';
--- gerenciamento/src/styles/global.js
+++ gerenciamento/src/styles/global.js
@@ -6,6 +6,7 @@ export const GlobalStyles = createGlobalStyle`
 body {
     margin: 0;
     padding: 0;
+    background-color: #F5F5F5;
 }
 
 * {
