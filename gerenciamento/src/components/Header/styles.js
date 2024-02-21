3e57d053d08a63595881272c37c72e910ad4e0d8
camilagsoares <camila.soares19@hotmail.com>
2024-02-21 12:44:42 -0300

    align itens header

--- gerenciamento/src/components/Header/styles.js
+++ gerenciamento/src/components/Header/styles.js
@@ -10,7 +10,8 @@ box-shadow: 0 1px 3px rgba(0,0,0,0.12), 0 1px 2px rgba(0,0,0,0.24);
 
 export const AlignItems = styled.div `
 display: flex;
-justify-content: space-between;
+align-items: center;
+margin-left: auto;
 `
 
 export const AlignItemsStart = styled.div`
