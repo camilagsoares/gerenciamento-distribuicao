bb795f89b80d1ec445c9cc0bf36caf787f1f8d2e
camilagsoares <camila.soares19@hotmail.com>
2024-02-26 13:52:01 -0300

    change layout

--- gerenciamento/src/components/Layout/index.js
+++ gerenciamento/src/components/Layout/index.js
@@ -5,12 +5,6 @@ import React from "react";
 
 export default function Layout() {
     return (
-        // <div className=" h-screen w-screen overflow-hidden flex flex-row">
-        //     <Header />
-        //     <div className="flex-1 p-4 min-h-0 overflow-auto">
-        //         <Outlet />
-        //     </div>
-        // </div>
         <div style={{ display: "flex", height: "100%" }}>
         <Header />
         <div style={{ flex: 1, overflowY: "auto",marginTop: "100px" }}>
