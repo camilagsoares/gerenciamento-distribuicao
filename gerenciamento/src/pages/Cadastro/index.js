f2260bd038a379eb14ac8f6686e7d89cd7f7643f
camilagsoares <camila.soares19@hotmail.com>
2024-02-29 11:48:16 -0300

    page signUp

--- gerenciamento/src/pages/Cadastro/index.js
+++ gerenciamento/src/pages/Cadastro/index.js
@@ -1,7 +1,119 @@
-import React from "react";
+import * as React from 'react';
+import Avatar from '@mui/material/Avatar';
+import { Link } from 'react-router-dom'
+import Grid from '@mui/material/Grid';
+import Box from '@mui/material/Box';
+import LockOutlinedIcon from '@mui/icons-material/LockOutlined';
+import { ContainerSignUp, AlignContainer } from './style'
+import { Button, FormRow, InputData, Input, Underline, Label, BoxButton, } from './style';
+
 
 export const Cadastro = () => {
-    return(
-        <div>Cadastro</div>
-    )
+
+
+  return (
+    <div style={{ background: 'linear-gradient(to right, #007bff, #0056b3)', minHeight: '100vh' }}>
+      <AlignContainer>
+        <ContainerSignUp>
+
+          <Box
+            sx={{
+              // marginTop: 8,
+              display: 'flex',
+              flexDirection: 'column',
+              alignItems: 'center',
+            }}
+          >
+            <Box sx={{
+              display: 'flex',
+              justifyContent: 'space-between'
+            }}>
+
+              <Avatar sx={{ m: 1, bgcolor: "#007BFF", marginTop: '25px', size: '40px', height: '40px', width: '40px' }}>
+                <LockOutlinedIcon />
+              </Avatar>
+
+              <h1>Cadastro</h1>
+            </Box>
+
+
+          </Box>
+          <FormRow>
+            <Grid container spacing={2}>
+              <Grid item xs={12} sm={6}>
+                <InputData>
+                  <Input type="text" required />
+                  <Underline />
+                  <Label>Nome</Label>
+                </InputData>
+              </Grid>
+
+              <Grid item xs={12} sm={6}>
+
+                <InputData>
+                  <Input type="text" required />
+                  <Underline />
+                  <Label>Email</Label>
+                </InputData>
+              </Grid>
+
+              <Grid item xs={12} sm={6}>
+                <InputData>
+                  <Input type="password" required />
+                  <Underline />
+                  <Label>Senha</Label>
+                </InputData>
+              </Grid>
+
+              <Grid item xs={12} sm={6}>
+
+                <InputData>
+                  <Input type="text" required />
+                  <Underline />
+                  <Label>Matrícula</Label>
+                </InputData>
+              </Grid>
+              <Grid item xs={12} sm={6}>
+
+                <InputData>
+                  <Input type="text" required />
+                  <Underline />
+                  <Label>Telefone</Label>
+                </InputData>
+              </Grid>
+
+              <Grid item xs={12} sm={6}>
+
+                <InputData>
+                  <Input type="text" required />
+                  <Underline />
+                  <Label>Departamento</Label>
+                </InputData>
+              </Grid>
+            </Grid>
+          </FormRow>
+
+          <BoxButton>
+            <Button>Cadastrar</Button>
+          </BoxButton>
+
+          <Grid container justifyContent="center" marginTop="10px">
+            <Grid item>
+              <Link to="/login"
+              >
+
+                <span style={{
+                  color: 'black',
+                  fontFamily: 'Poppins',
+                  textDecoration: 'none',
+                  fontSize: '15px'
+                }}> Já possui uma conta? Entrar</span>
+              </Link>
+            </Grid>
+          </Grid>
+        </ContainerSignUp>
+
+      </AlignContainer>
+    </div>
+  )
 }
---
+++ gerenciamento/src/pages/Cadastro/style.js
@@ -0,0 +1,105 @@
+import styled from "styled-components";
+
+
+export const AlignContainer = styled.div`
+display: flex;
+align-items: center;
+justify-content: center;
+height: 100vh; 
+`
+
+
+export const ContainerSignUp = styled.div`
+background-color: #fff;
+border-radius: 8px;
+box-shadow: 0px 0px 10px rgba(0, 0, 0, 0.1);
+max-height: 500px;
+width: 70%;
+height: 460px;
+
+h1 {
+    font-family: 'Poppins';
+    font-weight: 500;
+    padding: 0 20px;
+  }
+`
+
+
+export const Button = styled.button`
+  width: 93%;
+  background-color: #007bff;
+  color: #fff;
+  border: none;
+  border-radius: 4px;
+  height: 44px;
+  cursor: pointer;
+  font-family: 'Poppins';
+  font-size: 16px;
+  line-height: 1;
+  font-weight: 400;
+  transition: background-color 1s;
+  height: 50px;
+
+  &:hover{
+    background-color: #0059B8;
+  }
+`;
+
+
+export const InputData = styled.div`
+  width: 100%;
+  height: 40px;
+  padding: 0 20px;
+  /* margin: 0 20px; */
+  position: relative;
+  font-family: 'Poppins';
+  margin-bottom: 20px; 
+`;
+
+export const Underline = styled.div`
+  position: absolute;
+  bottom: 0;
+  height: 2px;
+  width: 100%;
+`;
+
+export const Input = styled.input`
+  display: block;
+  width: 100%;
+  height: 100%;
+  border: none;
+  font-size: 17px;
+  border-bottom: 2px solid rgba(0,0,0, 0.12);
+  outline: none;
+  font-family: 'Poppins';
+
+  &:focus ~ label,
+  &:valid ~ label {
+    transform: translateY(-20px);
+    font-size: 14px;
+    color: #3498db;
+  }
+`;
+
+export const Label = styled.label`
+  position: absolute;
+  pointer-events: none;
+  bottom: 10px;
+  font-size: 16px;
+  transition: all 0.3s ease;
+`;
+
+
+
+export const FormRow = styled.div`
+  display: flex;
+  flex-direction: column;
+  margin: 32px 0;
+`;
+
+export const BoxButton = styled.div`
+display: flex;
+align-items: center;
+justify-content: center;
+`
+
