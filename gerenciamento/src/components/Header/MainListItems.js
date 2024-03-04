95c2b01f5697058a6759ed994439e194b57200ed
camilagsoares <camila.soares19@hotmail.com>
2024-03-04 09:06:45 -0300

    change listItem and add tooltip

---
+++ gerenciamento/src/components/Header/MainListItems.js
@@ -0,0 +1,66 @@
+import * as React from 'react';
+import ListItemButton from '@mui/material/ListItemButton';
+import ListItemIcon from '@mui/material/ListItemIcon';
+import ListItemText from '@mui/material/ListItemText';
+import ListSubheader from '@mui/material/ListSubheader';
+import DashboardIcon from '@mui/icons-material/Dashboard';
+import ShoppingCartIcon from '@mui/icons-material/ShoppingCart';
+import PeopleIcon from '@mui/icons-material/People';
+import BarChartIcon from '@mui/icons-material/BarChart';
+import LayersIcon from '@mui/icons-material/Layers';
+import AssignmentIcon from '@mui/icons-material/Assignment';
+import { NavLink } from 'react-router-dom';
+import { Tooltip } from "@mui/material"
+
+export const MainListItems = ({open}) => {
+  return (
+    <React.Fragment>
+
+      <Tooltip placement="right-start" title={!open ? "Início" : ""}>
+        <NavLink to='/' style={{ textDecoration: 'none', color: 'black' }}>
+          <ListItemButton>
+            <ListItemIcon>
+              <DashboardIcon />
+            </ListItemIcon>
+            <ListItemText primary="Início" />
+          </ListItemButton>
+        </NavLink>
+      </Tooltip>
+
+      <Tooltip title={!open ? "Postar" : ""} placement="right-start">
+        <NavLink to='/postar' style={{ textDecoration: 'none', color: 'black' }}>
+          <ListItemButton>
+            <ListItemIcon>
+              <ShoppingCartIcon />
+            </ListItemIcon>
+            <ListItemText primary="Postar" />
+          </ListItemButton>
+        </NavLink>
+      </Tooltip>
+
+
+      <ListItemButton>
+        <ListItemIcon>
+          <PeopleIcon />
+        </ListItemIcon>
+        <ListItemText primary="Customers" />
+      </ListItemButton>
+
+      <ListItemButton>
+        <ListItemIcon>
+          <BarChartIcon />
+        </ListItemIcon>
+        <ListItemText primary="Reports" />
+      </ListItemButton>
+
+      <ListItemButton>
+        <ListItemIcon>
+          <LayersIcon />
+        </ListItemIcon>
+        <ListItemText primary="Integrations" />
+      </ListItemButton>
+
+    </React.Fragment>
+  )
+}
+
