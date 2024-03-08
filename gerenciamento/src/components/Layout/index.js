f4dbc6a31286266f373941a6766cf6630db58209
camilagsoares <camila.soares19@hotmail.com>
2024-03-08 08:36:38 -0300

    layout and outlet

--- gerenciamento/src/components/Layout/index.js
+++ gerenciamento/src/components/Layout/index.js
@@ -2,7 +2,6 @@ import { Outlet } from "react-router-dom";
 import Header from "../../components/Header/index"
 import * as React from "react";
 
-
 export default function Layout() {
     return (
         <div style={{ display: "flex", height: "100%" }}>
