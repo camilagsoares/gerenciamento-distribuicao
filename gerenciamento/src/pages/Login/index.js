a9cd06385772c2f94e460c674293dd7fe54e0b6e
camilagsoares <camila.soares19@hotmail.com>
2024-03-21 11:44:16 -0300

    Funcionalidade login

--- gerenciamento/src/pages/Login/index.js
+++ gerenciamento/src/pages/Login/index.js
@@ -1,33 +1,85 @@
-import React from 'react';
-import { Button, LeftPanel, Container, RightPanel, LoginForm, FormRow, InputData, Input, Underline, Label,BoxButton } from './styles';
-
-
+import React, { useContext, useEffect, useState } from 'react';
+import { Button, LeftPanel, Container, RightPanel, LoginForm, FormRow, InputData, Input, Underline, Label, BoxButton } from './styles';
+import * as yup from 'yup';
+import { yupResolver } from '@hookform/resolvers/yup';
+import { useForm } from "react-hook-form";
+import { useApiLogin } from '../../services/api';
+import TextField from '@mui/material/TextField';
+import Grid from '@mui/material/Grid';
+import { Box } from '@mui/material';
+// import { AuthContext } from '../../contexts/auth.content'
+import { useNavigate } from 'react-router-dom';
+import axios from 'axios';
+import { toast, ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 const Login = () => {
+  const navigate = useNavigate();
+  const [email, setEmail] = useState('');
+  const [senha, setSenha] = useState('');
+
+  const handleLogin = async () => {
+    try {
+      const response = await axios.post('http://10.1.0.187:3000/api/auth/login', {
+        email: email,
+        senha: senha
+      });
+
+      const { token } = response.data;
+
+
+      localStorage.setItem('token', token);
+
+      setEmail('');
+      setSenha('');
+
+      toast.success('Login realizado com sucesso!', {
+        autoClose: 3000
+      });
+
+      setTimeout(() => {
+        navigate("/")
+      }, 3000);
+    } catch (error) {
+      console.error('Erro ao fazer login:', error);
+    }
+  };
+
   return (
     <Container>
       <LeftPanel />
       <RightPanel>
+        {/* */}
+        {/* <div>
+          <h2>Login</h2>
+          <input type="text" placeholder="E-mail" value={email} onChange={(e) => setEmail(e.target.value)} />
+          <input type="password" placeholder="Senha" value={senha} onChange={(e) => setSenha(e.target.value)} />
+          <button onClick={handleLogin}>Login</button>
+          <ToastContainer />
+
+        </div> */}
         <LoginForm>
           <h1>Login</h1>
           <FormRow>
             <InputData>
-              <Input type="text" required />
+              <Input type="text" required value={email} onChange={(e) => setEmail(e.target.value)} />
               <Underline />
-              <Label>Nome</Label>
+              <Label>E-mail</Label>
             </InputData>
             <br /> <br />
             <InputData>
-              <Input type="password" required />
+              <Input type="password" required value={senha} onChange={(e) => setSenha(e.target.value)} />
               <Underline />
               <Label>Senha</Label>
             </InputData>
           </FormRow>
           <BoxButton>
-          <Button>Entrar</Button>
+            <Button onClick={handleLogin}>Entrar</Button>
           </BoxButton>
+          <ToastContainer />
 
         </LoginForm>
+
       </RightPanel>
     </Container>
   );
