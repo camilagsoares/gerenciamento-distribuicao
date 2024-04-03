2301e0ffcca51c8ed1f22ff53d0c68dbd51fd3ae
camilagsoares <camila.soares19@hotmail.com>
2024-04-03 12:54:43 -0300

    mudanças

--- gerenciamento/src/services/api.js
+++ gerenciamento/src/services/api.js
@@ -1,25 +1,25 @@
 import axios from 'axios';
 import { useState, useEffect } from 'react'
 import { toast } from 'react-toastify';
+// http://10.1.0.187:3000/api
 
-// const tokenInStorage = localStorage.getItem('token');
+const tokenInStorage = localStorage.getItem('token');
 
-const axiosApi = axios.create({
-    baseURL: `http://10.1.0.187:3000/api`,
-    headers: {
-        // Authorization: `Bearer ${tokenInStorage}`,
-        'Content-Type': 'application/json',
-    },
-})
-
-
-const configHeaders = () => ({
-    // Authorization: `Bearer ${tokenInStorage}`,
+const api = axios.create({
+  // baseURL: `http://10.1.0.187:3000/api`,
+  baseURL: `http://10.1.0.187:3000/api`,
+  headers: {
+    Authorization: `Bearer ${tokenInStorage}`,
     'Content-Type': 'application/json',
+  },
 });
 
+const configHeaders = () => ({
+  Authorization: `Bearer ${tokenInStorage}`,
+  'Content-Type': 'application/json',
+});
 
-const useApiLogin = (path) => {
+const useApiLogin = () => {
   const [loginData, setLoginData] = useState(null);
   const [error, setError] = useState(null);
   const [loading, setLoading] = useState(false);
@@ -28,7 +28,7 @@ const useApiLogin = (path) => {
     setError(null);
     setLoading(true);
     try {
-      const response = await axios.post(`http://10.1.0.187:3000/api${path}`, payload, {
+      const response = await axios.post(`http://10.1.0.187:3000/api/auth/login`, payload, {
         headers: {
           'Content-Type': 'application/json',
         },
@@ -39,24 +39,28 @@ const useApiLogin = (path) => {
         type: 'success',
       });
     } catch (error) {
-      toast(error.response, {
+      const errorMessage = error?.response ? error?.response?.data?.message : 'Ocorreu um erro durante o login';
+      console.log(errorMessage)
+      toast(errorMessage, {
         type: 'error',
       });
-      setError(error);
+      setError(error.message);
     } finally {
       setLoading(false);
     }
   };
 
+
   return { loginData, error, loading, handlerSubmitLogin: (payload) => login(payload) };
 };
 
 const useApiRequestGet = (path, payload) => {
-    const [data, setData] = useState(null);
-    const [error, setError] = useState(null);
-    const [loading, setLoading] = useState(false);
-  
-    const fetchData = async () => {
+  const [data, setData] = useState(null);
+  const [error, setError] = useState(null);
+  const [loading, setLoading] = useState(false);
+
+  useEffect(() => {
+    (async () => {
       try {
         setLoading(true);
         const response = await axios.get(`http://10.1.0.187:3000/api${path}`, {
@@ -65,40 +69,6 @@ const useApiRequestGet = (path, payload) => {
         });
         setData(response.data.content);
       } catch (error) {
-        setError(error.message);
-      } finally {
-        setLoading(false);
-      }
-    };
-  
-    useEffect(() => {
-      fetchData();
-    }, []);
-  
-    return { data, error, loading, refetchData: fetchData };
-  };
-
-  const useApiRequestSubmit = (method = 'post' | 'delete' | 'put', path, callback) => {
-    const [data, setData] = useState(null);
-    const [error, setError] = useState(null);
-    const [loading, setLoading] = useState(false);
-  
-    const submit = async () => {
-      try {
-        setLoading(true);
-        const response = await axios({
-          method,
-          url: `http://10.1.0.187:3000/api${path}`,
-        });
-  
-        setData(response.data);
-        // window.location.reload()
-  
-        // Chame o callback após a conclusão bem-sucedida do POST ou PUT
-        if (callback) {
-          callback(response.data);
-        }
-      } catch (error) {
         toast(error.response.data.message, {
           type: 'error',
         });
@@ -106,10 +76,40 @@ const useApiRequestGet = (path, payload) => {
       } finally {
         setLoading(false);
       }
-    };
-  
-    return { data, error, loading, handleSubmitData: () => submit() };
-};
-  
+    })();
+  }, []);
 
-  export { axiosApi, useApiRequestGet, useApiRequestSubmit , useApiLogin};
+
+  return { data, error, loading };
+};
+
+const useApiRequestSubmit = (method = 'post' | 'delete' | 'put', path) => {
+  const [data, setData] = useState(null);
+  const [error, setError] = useState(null);
+  const [loading, setLoading] = useState(false);
+
+  const submit = async (payload) => {
+    try {
+      setLoading(true);
+      const response = await axios({
+        method,
+        url: `http://10.1.0.187:3000/api${path}`,
+        data: payload,
+        headers: configHeaders(),
+      });
+
+      setData(response.data);
+    } catch (error) {
+      toast(error.response.data.message, {
+        type: 'error',
+      });
+      setError(error.message);
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  return { data, error, loading, handleSubmitData: (payload) => submit(payload) };
+};
+
+export { useApiLogin, useApiRequestGet, useApiRequestSubmit, api }
